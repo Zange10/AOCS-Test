d269ec876408bd4e5e58dc2b82e07a2a7e1177fd
Zange10 <niklaszan@protonmail.com>
2023-02-12 17:25:45 +0100

    polygons are shown, but seems wrong

--- main.c
+++ main.c
@@ -29,17 +29,23 @@ int xg = 1,yg = 1,wg = 1,hg = 1;
 int WINDOW_WIDTH = 1000, WINDOW_HEIGHT = 1000;
 double DISTANCE = 1000;
 int frames = 0;
+gboolean run = TRUE;
 const int FPS = 60;
 double lasttime;
 time_t rawtime;
 
 Cube cube = {100,0,0, 0, 400,0,0};
 
-//Point3D origin = {350,190,0};
+//Point3D observer = {350,190,0};
+Point3D observer = {0,0,0};
 Point3D origin = {0,0,0};
 Vector looking = {1,0,0};
 Point3D lightsource = {0,-500,0};
 
+
+void print_vector(Vector v) {
+    printf("[%lf; %lf; %lf]\n", v.x, v.y, v.z);
+}
 void print_point(Point3D point) {
     printf("[%lf; %lf; %lf]\n", point.x, point.y, point.z);
 }
@@ -63,6 +69,28 @@ double dot_product(Vector v1, Vector v2) {
     return v1.x*v2.x + v1.y*v2.y + v1.z*v2.z;
 }
 
+Vector cross_product(Vector v1, Vector v2) {
+    Vector v;
+    v.x = v1.y*v2.z-v1.z*v2.y;
+    v.y = v1.z*v2.x-v1.x*v2.z;
+    v.z = v1.x*v2.y-v1.y*v2.x;
+    return v;
+}
+
+Vector scalar_multiplication(double s, Vector v) {
+    v.x *= s;
+    v.y *= s;
+    v.z *= s;
+    return v;
+}
+
+// v1 on v2
+Vector get_vector_projection(Vector v1, Vector v2) {
+    double v2_length = get_vector_length(v2);
+    double scalar = dot_product(v1, v2) / (v2_length*v2_length);
+    return scalar_multiplication(scalar, v2);
+}
+
 Vector normalize_vector(Vector v) {
     double length = get_vector_length(v);
     v.x /= length;
@@ -77,8 +105,78 @@ double get_vector_angle(Vector v1, Vector v2) {
 
 Point2D p3d_to_p2d(Point3D p3d) {
     Point2D p2d;
-    p2d.x = (p3d.y-origin.y)/(p3d.x-origin.x) * DISTANCE + WINDOW_WIDTH/2;
-    p2d.y = -(p3d.z-origin.z)/(p3d.x-origin.x) * DISTANCE + WINDOW_HEIGHT/2;
+    Vector v3d = getVector(observer, p3d);
+    Vector proj_p3d = get_vector_projection(v3d, looking);
+    Vector observer_plane_xy = {0,0,0};
+    DISTANCE = 200;
+
+    if(looking.x != 0 && looking.y != 0) {
+        double observer_plane_y = - looking.x/looking.y;
+        observer_plane_xy.x = 1;
+        observer_plane_xy.y = observer_plane_y;
+    } else if (looking.x == 0) {
+        observer_plane_xy.x = 1;
+    } else if (looking.y == 0) {
+        observer_plane_xy.y = 1;
+    }
+    Vector observer_plane_xyz = cross_product(looking,observer_plane_xy);
+    Vector nadir = {p3d.x-proj_p3d.x, p3d.y-proj_p3d.y, p3d.z-proj_p3d.z};
+    print_point(observer);
+    print_vector(looking);
+    print_vector(observer_plane_xy);
+    print_vector(observer_plane_xyz);
+    printf("--\n");
+    print_point(p3d);
+    print_vector(proj_p3d);
+    print_vector(nadir);
+    printf("--\n");
+
+    Vector plane_proj_x = get_vector_projection(nadir,observer_plane_xy);
+    Vector plane_proj_y = get_vector_projection(nadir,observer_plane_xyz);
+    print_vector(plane_proj_x);
+    print_vector(plane_proj_y);
+    printf("--\n");
+
+    double a = 0;
+    double s = 0;
+    double nadir_length = get_vector_length(nadir);
+    
+    if(nadir_length != 0) {
+        a = get_vector_angle(observer_plane_xy,nadir);
+        printf("%lf°\n", get_vector_angle(observer_plane_xyz,nadir));
+        s = (DISTANCE/cos(a))/get_vector_length(v3d);
+    }
+    
+    printf("%lf =  %lf / %lf ## a: %lf\n", s,(DISTANCE/cos(a)), get_vector_length(v3d), a);
+
+    p2d.y = s*sin(a) * DISTANCE + WINDOW_HEIGHT/2;
+    if(get_vector_angle(observer_plane_xyz,nadir) < M_PI/2){
+        if(get_vector_angle(observer_plane_xy,nadir) < M_PI/2){
+            p2d.x = fabs(s*cos(a)) * DISTANCE + WINDOW_WIDTH/2;
+            p2d.y = -fabs(s*sin(a)) * DISTANCE + WINDOW_HEIGHT/2;
+        } else {
+            p2d.x = -fabs(s*cos(a)) * DISTANCE + WINDOW_WIDTH/2;
+            p2d.y = -fabs(s*sin(a)) * DISTANCE + WINDOW_HEIGHT/2;
+        }
+    } else {
+        if(get_vector_angle(observer_plane_xy,nadir) < M_PI/2){
+            p2d.x = fabs(s*cos(a)) * DISTANCE + WINDOW_WIDTH/2;
+            p2d.y = fabs(s*sin(a)) * DISTANCE + WINDOW_HEIGHT/2;
+        } else {
+            p2d.x = -fabs(s*cos(a)) * DISTANCE + WINDOW_WIDTH/2;
+            p2d.y = fabs(s*sin(a)) * DISTANCE + WINDOW_HEIGHT/2;
+        }
+    }
+
+    print_point2D(p2d);
+
+
+
+
+    //p2d.x = (p3d.y-observer.y)/(p3d.x-observer.x) * DISTANCE + WINDOW_WIDTH/2;
+    //p2d.y = -(p3d.z-observer.z)/(p3d.x-observer.x) * DISTANCE + WINDOW_HEIGHT/2;
+    print_point2D(p2d);
+    printf("\n");
     return p2d;
 }
 
@@ -132,7 +230,7 @@ void draw_face(cairo_t *cr, Point3D observer, Point3D CoM, Point3D points[4]) {
     center.z = (points[0].z + points[1].z + points[2].z + points[3].z)/4;
 
     Vector CoM2c = getVector(CoM,center);
-    Vector o2c = getVector(origin,center);
+    Vector o2c = getVector(observer,center);
     Vector ls2c = getVector(lightsource,center);
 
     double a = get_vector_angle(CoM2c,o2c);
@@ -145,7 +243,7 @@ void draw_face(cairo_t *cr, Point3D observer, Point3D CoM, Point3D points[4]) {
 
 
     for(int i = 0; i < 4; i++) {
-        Vector tempv = getVector(origin,points[i]);
+        Vector tempv = getVector(observer,points[i]);
         if(get_vector_angle(tempv,looking) > M_PI/2) return;
         p2d[i+2] = p3d_to_p2d(points[i]);
     }
@@ -200,24 +298,19 @@ void draw_skeleton(GtkWidget *widget, cairo_t *cr, gpointer data) {
 
 void draw_faces(cairo_t *cr, Cube cube) {
     Point3D * p3d = getCubePoints(cube.p,cube.length,cube.pitch,cube.yaw,cube.roll);
-    Point2D p2d[8];
     Point3D p_temp[4];
-
-    for(int i = 0; i < 8; i++) {
-        p2d[i] = p3d_to_p2d(p3d[i]);
-    }
 
     for(int i = 0; i < 2; i++) {
         for(int j = 0; j < 4; j++) {
             p_temp[j] = p3d[i+j*2];
         }
-        draw_face(cr,origin,cube.p,p_temp);
+        draw_face(cr,observer,cube.p,p_temp);
     }
     for(int i = 0; i < 2; i++) {
         for(int j = 0; j < 4; j++) {
             p_temp[j] = p3d[j+i*4];
         }
-        draw_face(cr,origin,cube.p,p_temp);
+        draw_face(cr,observer,cube.p,p_temp);
     }
     
     for(int i = 0; i < 2; i++) {
@@ -225,15 +318,17 @@ void draw_faces(cairo_t *cr, Cube cube) {
             p_temp[1] = p3d[2*i+1];
             p_temp[2] = p3d[2*i+4];
             p_temp[3] = p3d[2*i+5];
-        draw_face(cr,origin,cube.p,p_temp);
+        draw_face(cr,observer,cube.p,p_temp);
     }
 }
 
 static gboolean on_button_press_event(GtkWidget *widget, GdkEventButton *event, gpointer user_data) {
-    if (event->button == 3) {
-        g_print("Right mouse button pressed at (%f, %f, %d)\n", event->x, event->y, event->button);
-    }
+    mouse_pressed[event->button-1] = TRUE;
     return FALSE;
+}
+
+void on_button_release(GtkWidget *widget, GdkEventButton *event, gpointer user_data) {
+    mouse_pressed[event->button-1] = FALSE;
 }
 
 static gboolean on_key_press_event(GtkWidget *widget, GdkEventKey *event, gpointer user_data) {
@@ -266,7 +361,7 @@ static gboolean on_draw(GtkWidget *widget, cairo_t *cr, gpointer data)
     if(WINDOW_WIDTH<WINDOW_HEIGHT) DISTANCE = WINDOW_WIDTH;
     else DISTANCE = WINDOW_HEIGHT;
 
-    //lightsource = origin;
+    //lightsource = observer;
 
     //cube.pitch  += M_PI/200;
     //cube.yaw    += M_PI/250;
@@ -285,15 +380,16 @@ static gboolean on_draw(GtkWidget *widget, cairo_t *cr, gpointer data)
 static gboolean on_timeout(gpointer data)
 {
     double move = 2;
-    if(key_pressed[0]) origin.x += move;
-    if(key_pressed[1]) origin.x -= move;
-    if(key_pressed[2]) origin.y -= move;
-    if(key_pressed[3]) origin.y += move;
-    if(key_pressed[4]) origin.z -= move;
-    if(key_pressed[5]) origin.z += move;
+    if(key_pressed[0]) observer.x += move;
+    if(key_pressed[1]) observer.x -= move;
+    if(key_pressed[2]) observer.y -= move;
+    if(key_pressed[3]) observer.y += move;
+    if(key_pressed[4]) observer.z -= move;
+    if(key_pressed[5]) observer.z += move;
 
-    print_point(origin);
-
+    if(mouse_pressed[0]) printf("Left Mouse pressed\n");
+    if(mouse_pressed[1]) printf("Middle Mouse pressed\n");
+    if(mouse_pressed[2]) printf("Right Mouse pressed\n");
 
     rawtime = time(NULL);
     if(rawtime != lasttime) {
@@ -317,17 +413,17 @@ int main(int argc, char *argv[])
     gtk_window_set_title(GTK_WINDOW(window), "My GTK Window");
     gtk_window_set_default_size(GTK_WINDOW(window), WINDOW_WIDTH, WINDOW_HEIGHT);
     g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
-    g_signal_connect(window, "button-press-event", G_CALLBACK(on_button_press_event), NULL);
-    g_signal_connect(window, "button-press-event", G_CALLBACK(on_button_press_event), NULL);
     g_signal_connect(window, "key-press-event", G_CALLBACK(on_key_press_event), NULL);
     g_signal_connect(G_OBJECT(window), "key-release-event", G_CALLBACK(key_release_callback), NULL);
+    g_signal_connect(window, "button-press-event", G_CALLBACK(on_button_press_event), NULL);
+    g_signal_connect(window, "button-release-event", G_CALLBACK(on_button_release), NULL);
 
     drawing_area = gtk_drawing_area_new();
     g_signal_connect(drawing_area, "draw", G_CALLBACK(on_draw), NULL);
 
     gtk_container_add(GTK_CONTAINER(window), drawing_area);
     gtk_widget_show_all(window);
-    g_timeout_add(1.0/FPS*1000.0, on_timeout, drawing_area);
+    if(run) g_timeout_add(1.0/FPS*1000.0, on_timeout, drawing_area);
     gtk_main();
 
     return 0;
